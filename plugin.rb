a8ac8193072c1123831126eada27f3fcd27f0763
U-momon-PC\momon <muhlisbc@gmail.com>
2017-10-31 16:02:53 +0700

    make the cookie expire longer

--- plugin.rb
+++ plugin.rb
@@ -8,15 +8,17 @@ enabled_site_setting :images_guardian_enabled
 
 after_initialize {
 
-  require_dependency "session_controller"
-  class ::SessionController
+  require_dependency "application_controller"
+  class ::ApplicationController
     
     after_action :set_images_guardian_cookie
 
     def set_images_guardian_cookie
-      igc_val, igc_exp = ((!SiteSetting.images_guardian_enabled || (SiteSetting.images_guardian_enabled && current_user)) ? [ENV["IGUARD_COOKIE"], 1.year.from_now] : ["", 1.year.ago])
-
-      cookies[:iguard] = {value: igc_val, expires: igc_exp}
+      if (!SiteSetting.images_guardian_enabled || (SiteSetting.images_guardian_enabled && current_user))
+        cookies[:iguard] = {value: ENV["IGUARD_COOKIE"], expires: 5.years.from_now}
+      else
+        cookies.delete :iguard
+      end      
     end
 
   end
