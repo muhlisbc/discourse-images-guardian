1f78ba41aac3112bcd9050595a72e716af0bee02
U-momon-PC\momon <muhlisbc@gmail.com>
2017-10-31 15:12:34 +0700

    move the logic to session controller

--- plugin.rb
+++ plugin.rb
@@ -8,13 +8,13 @@ enabled_site_setting :images_guardian_enabled
 
 after_initialize {
 
-  require_dependency "application_controller"
-  class ::ApplicationController
+  require_dependency "session_controller"
+  class ::SessionController
     
     after_action :set_images_guardian_cookie
 
     def set_images_guardian_cookie
-      igc_val, igc_exp = ((!SiteSetting.images_guardian_enabled || (SiteSetting.images_guardian_enabled && current_user)) ? [ENV["IGUARD_COOKIE"], 1.minute.from_now] : ["", 1.minute.ago])
+      igc_val, igc_exp = ((!SiteSetting.images_guardian_enabled || (SiteSetting.images_guardian_enabled && current_user)) ? [ENV["IGUARD_COOKIE"], 1.year.from_now] : ["", 1.year.ago])
 
       cookies[:iguard] = {value: igc_val, expires: igc_exp}
     end
