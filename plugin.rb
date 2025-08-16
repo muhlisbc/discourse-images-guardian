7c8c6f9a84645878d916fc146253cc1b9398a152
Muhlis Cahyono <muhlisbc@gmail.com>
2025-08-16 12:40:16 +0700

    FIX: custom settings for logos

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-images-guardian
 # about: Discourse plugin that would prevent non-authenticated users from accessing uploaded images
-# version: 0.3.0
+# version: 0.4.0
 # authors: Muhlis Budi Cahyono (muhlisbc@gmail.com)
 # url: https://github.com/momon/discourse-images-guardian
 
@@ -29,43 +29,52 @@ after_initialize do
     end
 
   end
-  # require_dependency "site_setting"
-  # class ::SiteSetting
-  #   class << self
 
-  #     {
-  #       #site_home_logo_url: "logo",
-  #       site_logo_url: "logo",
-  #       site_logo_small_url: "logo_small",
-  #       site_digest_logo_url: "digest_logo",
-  #       site_mobile_logo_url: "mobile_logo",
-  #       site_large_icon_url: "large_icon",
-  #       site_favicon_url: "favicon",
-  #       site_apple_touch_icon_url: "apple_touch_icon",
-  #       site_opengraph_image_url: "opengraph_image",
-  #       site_twitter_summary_large_image_url: "twitter_summary_large_image",
-  #       site_push_notifications_icon_url: "push_notifications_icon"
-  #     }.each do |m, trans|
+  require_dependency("site_setting")
 
-  #       I18n.backend.store_translations(:en, {
-  #         site_settings: {
-  #           "images_guardian_#{m}" => I18n.t("site_settings.#{trans}") + " <em>(original setting: <strong>#{trans}</strong>)</em>"
-  #         }
-  #       })
+  class ::SiteSetting
+    class << self
 
-  #       alias_method "orig_#{m}".to_sym, m
+      [
+        site_digest_logo_url,
+        :site_large_icon_url,
+        :site_manifest_icon_url,
+        :site_apple_touch_icon_url,
+        :site_x_summary_large_image_url,
+        :site_opengraph_image_url,
+        :site_push_notifications_icon_url,
+        :site_logo_url,
+        :site_logo_small_url,
+        :site_mobile_logo_url,
+        :site_favicon_url,
+        :site_logo_dark_url,
+        :site_logo_small_dark_url,
+        :site_mobile_logo_dark_url
+      ].each do |m|
 
-  #       define_method(m) do
-  #         custom_setting = SiteSetting.send("images_guardian_#{m}")
-  #         original_setting = SiteSetting.send("orig_#{m}")
+        trans = m.to_s.gsub(/site\_/, "").gsub(/\_url/, "")
 
-  #         return original_setting if (!SiteSetting.images_guardian_enabled || custom_setting.blank?)
+        I18n.backend.store_translations(
+          :en,
+          {
+            site_settings: {
+              "images_guardian_#{m}" => I18n.t("site_settings.#{trans}") + " <em>(original setting: <strong>#{trans}</strong>)</em>"
+            }
+          }
+        )
 
-  #         custom_setting
-  #       end
+        alias_method "orig_#{m}".to_sym, m
 
-  #     end
+        define_method(m) do
+          custom_setting = SiteSetting.send("images_guardian_#{m}")
+          original_setting = SiteSetting.send("orig_#{m}")
 
-  #   end
-  # end
+          return original_setting if (!SiteSetting.images_guardian_enabled || custom_setting.blank?)
+
+          custom_setting
+        end
+      end
+
+    end
+  end
 end
